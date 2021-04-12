a960c15f52e703ef8366c301194e5459e21a8d45
BingJiaChen <b07901088@ntu.edu.tw>
2021-04-12 22:53:57 +0800

    ntuiot_2

---
+++ rpi/blink_old.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+import RPi.GPIO as GPIO
+import time
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(8,GPIO.OUT)
+    print("OK")
+
+def blink():
+    while True:
+        GPIO.output(8,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(8,GPIO.LOW)
+        time.sleep(1)
+
+if __name__=="__main__":
+    setup()
+    blink()
